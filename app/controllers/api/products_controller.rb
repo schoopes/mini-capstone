62127da6a42a71bcbe9754d060253bd1e198790e
schoopes <schoopes@gmail.com>
2019-01-17 10:26:04 -0800

    created first route, controller, view

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def show_products
+    @products = Product.all
+    render 'show_products.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    get "/products_url" => "products#show_products"
+  end
 end
